67f3857ae8bd6e2a872f983b937c3c11b832c4ed
Michael Pilquist <mpilquist@gmail.com>
2016-01-14 14:10:03 -0500

    Fix #3

---
+++ src/main/scala/scodec/interop/scalaz/ScalazInstances.scala
@@ -0,0 +1,25 @@
+package scodec.interop.scalaz
+
+import language.higherKinds
+
+import _root_.scalaz._
+
+import scodec.bits.BitVector
+import scodec._
+
+private[scalaz] abstract class ScalazInstancesLowPriority {
+
+  implicit final def DecoderSemigroupInstance[A](implicit A: Semigroup[A]): Semigroup[Decoder[A]] =
+    new DecoderSemigroup[A]()
+
+}
+
+private class DecoderSemigroup[A](implicit A: Semigroup[A]) extends Semigroup[Decoder[A]] {
+  def append(x: Decoder[A], y: => Decoder[A]) = new Decoder[A] {
+    private lazy val yy = y
+    def decode(bits: BitVector) = (for {
+      first <- x
+      second <- yy
+    } yield A.append(first, second)).decode(bits)
+  }
+}
